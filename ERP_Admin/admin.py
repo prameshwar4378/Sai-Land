b365bb22d889de6a868d460a00a2c2572cfe9c80
Rameshwar Pawar <prameshwar4378@gmail.com>
2025-02-27 17:26:13 +0530

    all model register to the default admin

--- ERP_Admin/admin.py
+++ ERP_Admin/admin.py
@@ -1,12 +1,19 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
+# from django.contrib import admin
+# from .models import *
+# # Register your models here.
  
-admin.site.register(Product)
-admin.site.register(Finance_Bank)
-admin.site.register(Insurance_Bank)
-admin.site.register(BreakdownType)
-admin.site.register(Breakdown)
-admin.site.register(AllocateDriverToVehicle)
-admin.site.register(FuelRecord)
-  
+# admin.site.register(Product)
+# admin.site.register(Finance_Bank)
+# admin.site.register(Insurance_Bank)
+# admin.site.register(BreakdownType)
+# admin.site.register(Breakdown)
+# admin.site.register(AllocateDriverToVehicle)
+# admin.site.register(FuelRecord)
+
+from django.contrib import admin
+from django.apps import apps
+
+# Get all models in the current app
+app = apps.get_app_config('ERP_Admin')  # replace 'your_app_name' with your actual app name
+for model in app.get_models():
+    admin.site.register(model)
